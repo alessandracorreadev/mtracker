c75dbcf4167d322e4fecde3380cf042cd3dc1a19
Alejandro Román alejandro.roman@hotmail.com.br <alejandro.roman@hotmail.com.br>
2026-03-12 20:41:55 -0300

    controllers criados

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,10 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def index
+    @expenses = current_user.expenses
+    @incomes = current_user.incomes
+    @investments = current_user.investments
+  end
 end
