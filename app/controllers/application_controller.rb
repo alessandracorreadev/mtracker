77a8b47639e10462f987e59ee9a83c44816ac4cd
Alessandra Correa <bkpalecorrea@gmail.com>
2026-03-17 12:58:08 -0300

    navbar e landingpage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
-<<<<<<< custom-devise
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-=======
   before_action :authenticate_user!, unless: :devise_controller?
->>>>>>> master
 
   protected
 
