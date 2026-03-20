7aa2528779faba8e548ee1d79ad019ba19db1311
paulo elisque <pcelisque@gmail.com>
2026-03-19 21:25:15 -0300

    seed correta

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -2,5 +2,4 @@ RubyLLM.configure do |config|
   config.openai_api_key = ENV["GITHUB_TOKEN"]
   config.openai_api_base = "https://models.inference.ai.azure.com"
   config.default_model = "gpt-4o"
-  config.default_params = { max_tokens: 2048 }
 end
